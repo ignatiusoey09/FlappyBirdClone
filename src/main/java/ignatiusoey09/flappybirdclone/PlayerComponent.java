026e9be39710f9073a381437883558bdfe4a4731
Ignatius Oey <oey.ignatius@gmail.com>
2024-12-06 18:32:12 +0800

    Add player component to handle player physics

--- src/main/java/ignatiusoey09/flappybirdclone/PlayerComponent.java
+++ src/main/java/ignatiusoey09/flappybirdclone/PlayerComponent.java
@@ -1,4 +1,32 @@
 package ignatiusoey09.flappybirdclone;
 
-public class PlayerComponent {
+import com.almasb.fxgl.core.math.Vec2;
+import com.almasb.fxgl.entity.component.Component;
+
+public class PlayerComponent extends Component {
+    private Vec2 acceleration = new Vec2(0, 3);
+    private boolean isFalling = true;
+
+
+    @Override
+    public void onUpdate(double tpf) {
+        if (isFalling) {
+            acceleration.y += tpf * 10;
+        }
+
+        if (acceleration.y < -5) {
+            acceleration.y = -5;
+        }
+        entity.translate(acceleration);
+        isFalling = true;
+    }
+
+    public void jump() {
+        acceleration.addLocal(0, -2);
+    }
+
+    public void stopVertical() {
+        isFalling = false;
+        acceleration.setZero();
+    }
 }
