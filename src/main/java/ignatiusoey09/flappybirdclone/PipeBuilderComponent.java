09f7b18ff2602064a9e455335d9d83cb2cbf488a
Ignatius Oey <oey.ignatius@gmail.com>
2024-12-10 22:00:58 +0800

    Add random generation of pipes

--- src/main/java/ignatiusoey09/flappybirdclone/PipeBuilderComponent.java
+++ src/main/java/ignatiusoey09/flappybirdclone/PipeBuilderComponent.java
@@ -1,37 +1,46 @@
 package ignatiusoey09.flappybirdclone;
 
 import com.almasb.fxgl.core.math.Vec2;
-import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.component.Component;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 
+import java.util.Random;
+
 import static com.almasb.fxgl.dsl.FXGL.*;
 
 public class PipeBuilderComponent extends Component {
+
+    /**
+     * Handles Pipe entity translation to the left
+     */
     private class PipeComponent extends Component {
-        private Vec2 velocity = new Vec2(-5.0, 0);
+        private final Vec2 velocity = new Vec2(-5.0, 0);
         @Override
         public void onUpdate(double tpf) {
             entity.translate(velocity);
         }
     }
 
-    private final double DISTANCE = 100.0; //the distance between pipes
-    private final double INTERVAL = 1;
-    private double prev = getGameTimer().getNow();
+    private final Random random = new Random();
+    private final double HEIGHT = getAppHeight();
+    private final double DISTANCE = 140.0; //the distance between pipes
+
+    private final double BUILD_INTERVAL = 1.5; //time interval between pipe spawns
+    private double prevPipeSpawn = getGameTimer().getNow();
 
     @Override
     public void onUpdate(double tpf) {
-        if (getGameTimer().getNow() - prev >= INTERVAL) {
+        if (getGameTimer().getNow() - prevPipeSpawn >= BUILD_INTERVAL) {
             buildPair();
-            prev = getGameTimer().getNow();
+            prevPipeSpawn = getGameTimer().getNow();
         }
     }
 
     private void buildPair() {
-        double height = FXGL.getAppHeight();
-        Rectangle topRect = new Rectangle(60, (height - DISTANCE)/2);
+        double gapHeight = random.nextDouble(HEIGHT - DISTANCE - 80) + 40;
+
+        Rectangle topRect = new Rectangle(60, gapHeight);
         entityBuilder()
                 .type(EntityType.PIPE)
                 .at(getAppWidth(), 0)
@@ -40,10 +49,10 @@ public class PipeBuilderComponent extends Component {
                 .collidable()
                 .buildAndAttach();
 
-        Rectangle botRect = new Rectangle(60, (height - DISTANCE)/2, Paint.valueOf("#eb4034"));
+        Rectangle botRect = new Rectangle(60, HEIGHT - DISTANCE - gapHeight, Paint.valueOf("#eb4034"));
         entityBuilder()
                 .type(EntityType.PIPE)
-                .at(getAppWidth(), height/2 + DISTANCE)
+                .at(getAppWidth(), gapHeight + DISTANCE)
                 .with(new PipeComponent())
                 .viewWithBBox(botRect)
                 .collidable()
