2d16b74a604bc3f74017fc7b3f35acfc37f02268
Ignatius Oey <oey.ignatius@gmail.com>
2024-12-28 23:10:12 +0800

    Add UI controller for gameover screen

---
+++ src/main/java/ignatiusoey09/flappybirdclone/GameoverController.java
@@ -0,0 +1,14 @@
+package ignatiusoey09.flappybirdclone;
+
+import com.almasb.fxgl.ui.UIController;
+import javafx.fxml.FXML;
+
+public class GameoverController implements UIController {
+
+    @FXML
+    private int score;
+
+    @Override
+    public void init() {
+    }
+}
